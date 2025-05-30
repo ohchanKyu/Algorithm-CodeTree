58fb5a39939c480d45f96b421702359cee017761
Chan <okc0202@naver.com>
2025-05-30 16:35:48 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250530/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+typedef pair<int,int> coord;
+int check[1000001];
+int N;
+
+int main() {
+    
+    cin >> N;
+    
+    queue<coord> q;
+    q.push(coord(N,0));
+    check[N] = true;
+
+    while(!q.empty()){
+        coord now = q.front();
+        q.pop();
+        int now_x = now.first;
+        int count = now.second;
+        if (now_x == 1){
+            cout << count;
+            break;
+        }
+        if (now_x % 3 == 0){
+            int next_x = now_x /3;
+            if (next_x <= 0 || check[next_x]) continue;
+            check[next_x] = true;
+            q.push(coord(next_x,count+1));
+        }
+        if (now_x % 2 == 0){
+            int next_x = now_x / 2;
+            if (next_x <= 0 || check[next_x]) continue;
+            check[next_x] = true;
+            q.push(coord(next_x,count+1));
+        }
+        int next_x = now_x + 1;
+        if (next_x <= 1000000 && !check[next_x]){
+            check[next_x] = true;
+            q.push(coord(next_x,count+1));
+        }
+        next_x = now_x - 1;
+        if (next_x >= 1 && !check[next_x]){
+            check[next_x] = true;
+            q.push(coord(next_x,count+1));
+        }
+    }
+    
+    return 0;
+}
