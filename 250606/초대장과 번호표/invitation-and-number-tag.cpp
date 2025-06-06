11769e39957a7e0c5b598e5c88f60414ca1b0d5e
Chan <okc0202@naver.com>
2025-06-06 11:13:39 +0900

    Title: 초대장과 번호표, Time: 1070ms, Memory: 11MB, Status: Time Limit Exceed - Codetree

--- 250606/초대장과 번호표/invitation-and-number-tag.cpp
+++ 250606/초대장과 번호표/invitation-and-number-tag.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
-#include <set>
+#include <unordered_set>
 #include <vector>
 
 using namespace std;
 
 int N, G;
 
-typedef pair<int,set<int>> groupSet;
 
 int main() {
 
     cin >> N >> G;
    
-    vector<set<int>> groups(G);
+    vector<unordered_set<int>> groups(G);
     vector<bool> isFixed(G,false);
-    set<int> invited;
+    unordered_set<int> invited;
     invited.insert(1);
 
     for (int i = 0; i < G; i++) {
@@ -34,14 +33,14 @@ int main() {
         for(int i=0;i<G;i++){
             if (isFixed[i]) continue;
             int count = 0;
-            set<int> groupComponent = groups[i];
+            unordered_set<int> groupComponent = groups[i];
+            int target;
             for(int g : groupComponent){
                 if (invited.find(g) != invited.end()) count += 1;
+                else target = g;
             }
             if (count == groupComponent.size() - 1){
-                for(int g : groupComponent){
-                    if (invited.find(g) == invited.end()) invited.insert(g);
-                }
+                invited.insert(target);
                 isFixed[i] = true;
                 isChange = true;
             }
