9ea7e528c9182d365c3ad8a99164ed0bf011c26f
Chan <okc0202@naver.com>
2025-06-06 14:41:42 +0900

    Title: top K 숫자, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250606/top K 숫자/top-k-elements.cpp
+++ 250606/top K 숫자/top-k-elements.cpp
@@ -11,12 +11,11 @@ int main() {
     cin >> n >> k;
     set<int> s;
 
-    set<int>::reverse_iterator it;
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
         s.insert(arr[i]);
     }
-    it = s.rbegin();
+    auto it = s.rbegin();
     for(int i=0;i<k;i++){
         cout << *it << " ";
         it--;
