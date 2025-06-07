f5c8eecc93db1b0ee942dcfa50bc2ec97c088bf5
Chan <okc0202@naver.com>
2025-06-07 22:35:33 +0900

    Title: 최솟값 3개, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250607/최솟값 3개/top-3-smallest-number.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int n;
+int arr[100000];
+
+int main() {
+
+    cin >> n;
+    priority_queue<int,vector<int>,greater<int>> pq;
+
+    for (int i = 1; i <= n; i++) {
+        cin >> arr[i];
+        pq.push(arr[i]);
+        if (i < 3){
+            cout << -1 << "\n";
+            continue;
+        }
+        int ans = 1;
+        int first = pq.top(); pq.pop();
+        int second = pq.top(); pq.pop();
+        int third = pq.top(); pq.pop();
+        cout << first * second * third << "\n";
+        pq.push(first); pq.push(second); pq.push(third);
+    }
+    
+
+
+    return 0;
+}
