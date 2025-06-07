8652396c8b034343e157c906061d957c866756ab
Chan <okc0202@naver.com>
2025-06-07 16:28:51 +0900

    Title: 중앙값, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250607/중앙값/median.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+int t;
+int m;
+int arr[100000];
+vector<int> v;
+
+int main() {
+
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        cin >> m;
+        for (int j = 0; j < m; j++) {
+            cin >> arr[j];
+            v.push_back(arr[j]);
+            if (arr[j] % 2 == 1){
+                sort(v.begin(),v.end());
+                int index = (j+1) / 2;
+                cout << v[index] << " ";
+            }
+        }
+        cout << "\n";
+        v.clear();
+    }
+    return 0;
+}
