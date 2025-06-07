4f128e2c4e919234f07d1e7aef576558866f7d72
Chan <okc0202@naver.com>
2025-06-07 10:16:17 +0900

    Title: 최대로 연속한 숫자, Time: 709ms, Memory: 80MB, Status: Memory Limit Exceed - Codetree

---
+++ 250607/최대로 연속한 숫자/maximum-consecutive-number.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int n, m;
+int nums[100000];
+
+int main() {
+
+    set<int> s;
+
+    cin >> n >> m;
+    for(int i=0;i<=n;i++) s.insert(i);
+    
+    for (int i = 0; i < m; i++) {
+        cin >> nums[i];
+        s.erase(nums[i]);
+        int maxV = 0;
+        
+        auto it = s.begin();
+        int current = *it;
+        int currentCnt = 1;
+
+        for(;it != s.end();++it){
+            if (*it == current + 1){
+                currentCnt += 1;
+            }
+            else currentCnt = 1;
+            maxV = max(maxV,currentCnt);
+            current = *it;
+        }
+        cout << maxV << "\n";
+    }
+
+    return 0;
+}
