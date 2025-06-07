837e7c2c447e3b61eb75b352d947bcf4ba207ce2
Chan <okc0202@naver.com>
2025-06-07 11:04:05 +0900

    Title: 최대로 연속한 숫자, Time: 413ms, Memory: 10MB, Status: Passed - Codetree

--- 250607/최대로 연속한 숫자/maximum-consecutive-number.cpp
+++ 250607/최대로 연속한 숫자/maximum-consecutive-number.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <set>
+#include <tuple>
 
 using namespace std;
 
@@ -7,30 +8,30 @@ int n, m;
 
 int main() {
 
+    // 삭제된 숫자
     set<int> s;
+    // 길이, 시작 숫자, 끝 숫자
+    set<tuple<int,int,int>> s_len;
 
     cin >> n >> m;
-    for(int i=0;i<=n;i++) s.insert(i);
-    
-    for (int i = 0; i < m; i++) {
-        int x;
-        cin >> x;
-        s.erase(x);
-        int maxV = 0;
-        
-        auto it = s.begin();
-        int current = *it;
-        int currentCnt = 1;
+    s.insert(-1); s.insert(n+1);
+    s_len.insert(make_tuple(n+1,-1,n+1));
 
-        for(;it != s.end();++it){
-            if (*it == current + 1){
-                currentCnt += 1;
-            }
-            else currentCnt = 1;
-            maxV = max(maxV,currentCnt);
-            current = *it;
-        }
-        cout << maxV << "\n";
+    for (int i = 0; i < m; i++) {
+        int y;
+        cin >> y;
+        set<int>::iterator it;
+        it = s.upper_bound(y);
+        
+        int z = *it;
+        it --;
+        int x = *it;
+        
+        s.insert(y);
+        s_len.erase(make_tuple(z-x-1,x,z));
+        s_len.insert(make_tuple(y-x-1,x,y));
+        s_len.insert(make_tuple(z-y-1,y,z));
+        cout << get<0>(*s_len.rbegin()) << "\n";
     }
 
     return 0;
