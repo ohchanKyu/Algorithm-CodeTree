ecffffb4de10b57c4ba1a0dae20cbaf8ebb76190
Chan <okc0202@naver.com>
2025-06-07 16:43:52 +0900

    Title: 중앙값, Time: 47ms, Memory: 1MB, Status: Passed - Codetree

--- 250607/중앙값/median.cpp
+++ 250607/중앙값/median.cpp
@@ -1,30 +1,45 @@
 #include <iostream>
 #include <queue>
-#include <algorithm>
+#include <vector>
 
 using namespace std;
 
 int t;
 int m;
-int arr[100000];
-vector<int> v;
 
 int main() {
 
     cin >> t;
     for (int i = 0; i < t; i++) {
         cin >> m;
+        priority_queue<int> maxHeap; // Left 중간값보다 작거나 같은값 ( 내림차순 )
+        priority_queue<int,vector<int>,greater<int>> minHeap; // Right 중간값보다 큰 값 ( 오름차순 )
+        vector<int> ans;
+
         for (int j = 0; j < m; j++) {
-            cin >> arr[j];
-            v.push_back(arr[j]);
-            if ((j+1) % 2 == 1){
-                sort(v.begin(),v.end());
-                int index = (j+1) / 2;
-                cout << v[index] << " ";
+            
+            int x;
+            cin >> x;
+            
+            if (maxHeap.empty() || x <= maxHeap.top()){
+                maxHeap.push(x);
+            }else{
+                minHeap.push(x);
             }
+            
+            if (maxHeap.size() > minHeap.size() + 1){
+                minHeap.push(maxHeap.top());
+                maxHeap.pop();
+            }else if (minHeap.size() > maxHeap.size()){
+                maxHeap.push(minHeap.top());
+                minHeap.pop();
+            }
+            if ((j+1) % 2 == 1) ans.push_back(maxHeap.top());
+        }
+        for(int n : ans){
+            cout << n << " ";
         }
         cout << "\n";
-        v.clear();
     }
     return 0;
 }
