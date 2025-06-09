4ccfa5dc8f03f4244de9607f944b164f506499e6
Chan <okc0202@naver.com>
2025-06-09 12:43:07 +0900

    Title: 길이가 무한대인 수평선 트랙, Time: 63ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250609/길이가 무한대인 수평선 트랙/horizontal-track-with-infinite-length.cpp
+++ 250609/길이가 무한대인 수평선 트랙/horizontal-track-with-infinite-length.cpp
@@ -6,49 +6,30 @@
 using namespace std;
 
 int N, T;
-int start[100000];
-int speed[100000];
 
 
 int main() {
     
     cin >> N >> T;
+    set<int> s;
 
+    while(N--){
 
-    set<pair<int,int>> s;
-    map<int,int> m;
-    
-    for (int i = 0; i < N; i++) {
-        cin >> start[i] >> speed[i];
-        m[start[i]]= speed[i];
-        s.insert({start[i],speed[i]});
-    }
-
-    for(int t=0;t<T;t++){
-
-        auto it = s.begin();
-        map<int,int> new_m;
-        set<pair<int,int>> new_s;
-
-        for(;it != s.end();it++){
-            int current = (*it).first;
-            int speed = m[current];
-            int next = current + speed;
-            new_s.insert(make_pair(next, speed));
+        int a,b;
+        cin >> a >> b;
+        // T초후 최종 위치
+        int target = a + b * T;
+        while(1){
+            // target보다 크거나 같은 위치 모두 제거
+            // 앞지른 사람은 제거하기 위해
+            // 출발점이 앞선 사람의 정보가 먼저 주어져서 가능한 로직
+            auto it = s.lower_bound(target);
+            if (it == s.end()) break;
+            s.erase(it);
         }
-        it = new_s.begin();
-        // 위치에 맞는 속도 갱신
-        for(;it != new_s.end();it++){
-            int current = (*it).first;
-            int speed = (*it).second;
-            if (new_m.find(current) == new_m.end()) new_m[current] = speed;
-            else{
-                new_m[current] = min(new_m[current],speed);
-            }
-        }
-        m = move(new_m);
-        s = move(new_s);
+        // 해당 target을 그룹으로 등록
+        s.insert(target);
     }
-    cout << m.size();
+    cout << s.size();
     return 0;
 }
