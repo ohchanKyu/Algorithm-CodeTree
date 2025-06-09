68fce7e1524b0f9560a408ba24d60b9c6a024b12
Chan <okc0202@naver.com>
2025-06-09 19:28:31 +0900

    Title: 최단 거리 9, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250609/최단 거리 9/shortest-distance-9.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <limits.h>
+
+using namespace std;
+
+int n, m;
+int A, B;
+typedef pair<int,int> edge;
+int main() {
+
+    cin >> n >> m;
+    vector<vector<edge>> graph(n+1);
+    vector<bool> visited(n+1,false);
+    vector<int> mdist(n+1,INT_MAX);
+    vector<int> path(n+1);
+    priority_queue<edge,vector<edge>,greater<edge>> pq;
+
+    for (int i = 0; i < m; i++) {
+        int s,e,v;
+        cin >> s >> e >> v;
+        graph[s].push_back(edge(e,v));
+        graph[e].push_back(edge(s,v));   
+    }
+    cin >> A >> B;
+    mdist[A] = 0;
+    path[A] = 0;
+    pq.push(edge(0,A));
+
+    while(!pq.empty()){
+        edge now = pq.top(); pq.pop();
+        int x = now.second;
+        if (visited[x]) continue;
+        visited[x] = true;
+
+        for(edge next : graph[x]){
+            int nx = next.first;
+            int weight = next.second;
+            if (mdist[nx] > mdist[x] + weight){
+                mdist[nx] = mdist[x] + weight;
+                path[nx] = x;
+                pq.push(edge(mdist[nx],nx));
+            }
+        }
+    }
+    int x = B;
+    vector<int> vertices;
+    vertices.push_back(x);
+    while(x != A){
+        x = path[x];
+        vertices.push_back(x);
+    }
+    cout << mdist[B] << "\n";
+    for(int i= (int) vertices.size() -1;i>=0;i--){
+        cout << vertices[i] << " ";
+    }
+    return 0;
+}
