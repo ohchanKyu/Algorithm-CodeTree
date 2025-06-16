b884845866a5b0870f3d8599906d2f64a2b84a14
Chan <okc0202@naver.com>
2025-06-16 22:24:13 +0900

    Title: 알파벳과 사칙연산, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250616/알파벳과 사칙연산/calculations-with-alphabet.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string expression;
+vector<char> op;
+
+int maxV = 0;
+int num;
+
+void dfs(int index,vector<int> v){
+
+    if (index == num){
+        int val = v[0];
+        int cnt = 1;
+        for(char c : op){
+            if (c == '-'){
+                val -= v[cnt];
+            }else if (c == '+'){
+                val += v[cnt];
+            }else if (c == '*'){
+                val *= v[cnt];
+            }
+            cnt += 1;
+        }
+        maxV = max(maxV,val);
+        return;
+    }
+    for(int i=1;i<=4;i++){
+        v.push_back(i);
+        dfs(index+1,v);
+        v.pop_back();
+    }
+}
+int main() {
+
+    cin >> expression;
+    num = expression.length() / 2 + 1;
+    for(int i=0;i<expression.length();i++){
+        if(i % 2 == 1) op.push_back(expression[i]);
+    }
+    vector<int> v;
+    dfs(0,v);
+    cout << maxV;
+    return 0;
+}
