9010ad2c38e297c2cb12a7fb9cc9c6ce57984150
Chan <okc0202@naver.com>
2025-06-16 23:28:06 +0900

    Title: 알파벳과 사칙연산, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

--- 250616/알파벳과 사칙연산/calculations-with-alphabet.cpp
+++ 250616/알파벳과 사칙연산/calculations-with-alphabet.cpp
@@ -1,30 +1,39 @@
 #include <iostream>
-#include <string>
+#include <unordered_map>
 #include <vector>
 #include <limits.h>
+#include <set>
 
 using namespace std;
 
 string expression;
 vector<char> op;
+vector<char> charlist;
+
+set<char> s;
+unordered_map<char,int> m;
+bool alpha[6];
 
 int maxV = INT_MIN;
-int num;
+int num = 0;
 
-void dfs(int index,vector<int> v){
+void dfs(int index, vector<int> v){
 
     if (index == num){
-        int val = v[0];
-        int cnt = 1;
+        for(int i=0;i<v.size();i++){
+            m[charlist[i]] = v[i];
+        }
+        int val = m[expression[0]];
+        int cnt = 2;
         for(char c : op){
             if (c == '-'){
-                val -= v[cnt];
+                val -= m[expression[cnt]];
             }else if (c == '+'){
-                val += v[cnt];
+                val += m[expression[cnt]];
             }else if (c == '*'){
-                val *= v[cnt];
+                val *= m[expression[cnt]];
             }
-            cnt += 1;
+            cnt += 2;
         }
         maxV = max(maxV,val);
         return;
@@ -35,13 +44,19 @@ void dfs(int index,vector<int> v){
         v.pop_back();
     }
 }
+
 int main() {
 
     cin >> expression;
-    num = expression.length() / 2 + 1;
     for(int i=0;i<expression.length();i++){
         if(i % 2 == 1) op.push_back(expression[i]);
+        else s.insert(expression[i]);
     }
+    auto it = s.begin();
+    for(;it != s.end();it++){
+        charlist.push_back(*it);
+    }
+    num = s.size();
     vector<int> v;
     dfs(0,v);
     cout << maxV;
