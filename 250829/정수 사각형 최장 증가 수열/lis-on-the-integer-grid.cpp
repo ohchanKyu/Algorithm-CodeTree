f03ba54b7f405fbec8d325718739574b2be80f61
Chan <okc0202@naver.com>
2025-08-29 22:27:39 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 96ms, Memory: 2MB, Status: Passed - Codetree

--- 250829/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
+++ 250829/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
@@ -9,7 +9,7 @@ int dp[500][500];
 const int dx[] = {0,0,1,-1};
 const int dy[] = {1,-1,0,0};
 
-int go(int x, int y, int prev){
+int go(int x, int y){
 
     int &ret = dp[x][y];
     if (ret != -1) return ret;
@@ -19,8 +19,8 @@ int go(int x, int y, int prev){
         int nx = x + dx[d];
         int ny = y + dy[d];
         if (nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
-        if (grid[nx][ny] <= prev) continue;
-        ret = max(ret,go(nx,ny,grid[nx][ny]) + 1);
+        if (grid[nx][ny] <= grid[x][y]) continue;
+        ret = max(ret,go(nx,ny) + 1);
     }
     return ret;
 }
@@ -38,7 +38,7 @@ int main() {
     int maxV = 0;
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
-            maxV = max(maxV,go(i,j,-1));
+            maxV = max(maxV,go(i,j));
         }
     }
     cout << maxV;
