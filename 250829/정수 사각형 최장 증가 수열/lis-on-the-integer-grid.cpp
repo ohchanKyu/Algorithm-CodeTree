bbc9bd111673a782a46f61d245254f43a893aafd
Chan <okc0202@naver.com>
2025-08-29 22:21:20 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 68ms, Memory: 2MB, Status: Wrong Answer - Codetree

---
+++ 250829/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define prev nahc
+
+using namespace std;
+
+int n;
+int grid[500][500];
+int dp[500][500];
+const int dx[] = {0,0,1,-1};
+const int dy[] = {1,-1,0,0};
+
+int go(int x, int y, int prev){
+
+    int &ret = dp[x][y];
+    if (ret != -1) return ret;
+
+    ret = 1;
+    for(int d=0;d<4;d++){
+        int nx = x + dx[d];
+        int ny = y + dy[d];
+        if (nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
+        if (grid[nx][ny] <= prev) continue;
+        ret = max(ret,go(nx,ny,grid[nx][ny]) + 1);
+    }
+    return ret;
+}
+
+int main() {
+
+    cin >> n;
+
+    memset(dp,-1,sizeof(dp));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> grid[i][j];
+        }
+    }
+    cout << go(0,0,-1);
+    return 0;
+}
