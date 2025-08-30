cf2c6ae71e153bad4f2ae690ea64771608bec976
Chan <okc0202@naver.com>
2025-08-31 01:31:40 +0900

    Title: 벽이 있는 충돌 실험, Time: 145ms, Memory: 0MB, Status: Passed - Codetree

--- 250831/벽이 있는 충돌 실험/collision-experiment-with-wall.cpp
+++ 250831/벽이 있는 충돌 실험/collision-experiment-with-wall.cpp
@@ -12,7 +12,6 @@ int visited[54][54];
 struct Element{
     int x,y,d;
 };
-vector<Element> A[54][54];
 vector<Element> v;
 
 // 위쪽 오른쪽 아래 왼쪽
@@ -21,31 +20,30 @@ const int dy[] = {0,1,0,-1};
 
 void move() {
 
-    vector<vector<int>> next_dir(N, vector<int>(N, -1));  
+    memset(visited,-1,sizeof(visited));
     for (auto &it : v) {
         int x = it.x;
         int y = it.y;
         int dir = it.d;
         int nx = x + dx[dir];
         int ny = y + dy[dir];
-
         if (nx < 0 || ny < 0 || nx >= N || ny >= N) {
             dir = (dir + 2) % 4; 
             nx = x;
             ny = y;
         }
-        if (next_dir[nx][ny] == -1) {
-            next_dir[nx][ny] = dir; 
-        } else {
-            next_dir[nx][ny] = -2; 
+        if (visited[nx][ny] == -1){
+            visited[nx][ny] = dir;
+        }else{
+            visited[nx][ny] = -2;
         }
     }
 
     v.clear();
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            if (next_dir[i][j] != -1 && next_dir[i][j] != -2) {
-                v.push_back({i, j, next_dir[i][j]});
+            if (visited[i][j] != -1 && visited[i][j] != -2){
+                v.push_back({i,j,visited[i][j]});
             }
         }
     }
