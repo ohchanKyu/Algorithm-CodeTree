50970571f66831396642bf88f18fc43b9c0babf3
Chan <okc0202@naver.com>
2025-09-01 12:46:34 +0900

    Title: 핀볼게임, Time: 1088ms, Memory: 0MB, Status: Time Limit Exceed - Codetree

---
+++ 250901/핀볼게임/pinball-game.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+int grid[100][100]; 
+int pinbol[2][4] = {
+    {3,2,1,0},
+    {2,3,0,1}
+};
+int ret;
+// 상 하 좌 우
+const int dx[] = {-1,1,0,0};
+const int dy[] = {0,0,-1,1};
+
+int go(int x,int y,int dir){
+
+    int t = 0;
+    int sx = x;
+    int sy = y;
+    int sd = dir;
+    while(true){
+        int nx = x + dx[dir];
+        int ny = y + dy[dir];
+        if (nx < 0 || ny < 0 || nx >= n || ny >= n){
+            return t + 2;
+        }
+        if (grid[nx][ny] == 1){
+            dir = pinbol[0][dir];
+        }else if (grid[nx][ny] == 2){
+            dir = pinbol[1][dir];
+        }
+        x = nx;
+        y = ny;
+        t++;
+    }
+}
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++) cin >> grid[i][j];
+
+    for(int i=0;i<n;i++){
+        ret = max(ret,go(0,i,1));
+        ret = max(ret,go(i,0,3));
+        ret = max(ret,go(i,n-1,2));
+        ret = max(ret,go(n-1,i,0));
+    }
+    cout << ret;
+
+    return 0;
+}
