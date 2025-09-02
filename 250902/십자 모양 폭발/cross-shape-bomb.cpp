5d4c476e310cbfc6d4e3f0beffed0bb221fd328b
Chan <okc0202@naver.com>
2025-09-02 12:00:44 +0900

    Title: 십자 모양 폭발, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250902/십자 모양 폭발/cross-shape-bomb.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+int grid[200][200];
+int r, c;
+const int dx[] = {0,0,1,-1};
+const int dy[] = {1,-1,0,0};
+
+void downNumber(){
+    
+    for(int j=0;j<n;j++){
+        for(int i=n-2;i>=0;i--){
+            if (grid[i][j] == 0) continue;
+            int cx = i;
+            int cy = j;
+            while(true){
+                int nx = cx + 1;
+                int ny = cy;
+                if (nx < 0 || ny < 0 || nx >= n || ny >= n) break;
+                if (grid[nx][ny]) break;
+            }
+            grid[nx-1][ny] = grid[i][j];
+            grid[i][j] = 0;
+        }
+    }
+}
+void bomb(int r,int c){
+
+    int sx = r;
+    int sy = c;
+    int ends = grid[r][c];
+    grid[r][c] = 0;
+    for(int d=0;d<4;d++){
+
+        int x = sx;
+        int y = sy;
+        for(int cnt = 0; cnt < ends -1 ;cnt++){
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+            if (nx < 0 || ny < 0 || nx >= n || ny >= n) break;
+            grid[nx][ny] = 0;
+            x = nx;
+            y = ny;
+        }
+    }
+}
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    cin >> r >> c;
+    r--; c--;
+    bomb(r,c);
+    downNumber();
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << grid[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+
+    return 0;
+}
