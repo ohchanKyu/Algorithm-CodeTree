f6a5ce034a980e3d5eba2e12056f88de486004ed
Chan <okc0202@naver.com>
2025-10-08 10:47:42 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251008/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N,x,y;
+vector<pair<int,int>> v;
+int visited[1004];
+int dp[1004];
+
+int go(int idx){
+
+    if (idx == N) return 0;
+
+    int &ret = dp[idx];
+    if (ret != -1) return ret;
+    
+    ret = 0;
+    int s = v[idx].first;
+    int e = v[idx].second;
+    bool isPossible = true;
+    for(int i=s;i<=e;i++){
+        if (visited[i]){
+            isPossible = false;
+            break;
+        }
+    }
+    if (isPossible){
+        for(int i=s;i<=e;i++){
+            visited[i] = 1;
+        }
+        ret = max(ret,go(idx+1) + 1);
+        for(int i=s;i<=e;i++){
+            visited[i] = 0;
+        }
+    }else ret = max(ret,go(idx+1));
+    return ret;
+}
+
+int main(){
+
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> N;
+    for(int i=0;i<N;i++){
+        cin >> x >> y;
+        v.push_back({x,y});
+    }
+    memset(dp,-1,sizeof(dp));
+    cout << go(0);
+    return 0;
+}
