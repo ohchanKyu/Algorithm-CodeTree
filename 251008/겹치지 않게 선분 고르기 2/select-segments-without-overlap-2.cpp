e9f649e221b3e9dd332ea3d13e5a9978b7d3cc2c
Chan <okc0202@naver.com>
2025-10-08 11:03:15 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251008/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.cpp
+++ 251008/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.cpp
@@ -6,6 +6,11 @@ int N,x,y;
 vector<pair<int,int>> v;
 int ret;
 
+bool cmp(pair<int,int> &a,pair<int,int> &b){
+    
+    if (a.second == b.second) return a.first < b.first;
+    return a.second < b.second;
+}
 int main(){
 
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
@@ -14,7 +19,7 @@ int main(){
         cin >> x >> y;
         v.push_back({x,y});
     }
-    sort(v.begin(),v.end());
+    sort(v.begin(),v.end(),cmp);
     
     int l = 0;
     for(int i=0;i<N;i++){
