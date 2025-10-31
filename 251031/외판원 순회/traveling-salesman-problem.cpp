a6e97639e8cc491abb9dfbc20e6c718bddc017e7
Chan <okc0202@naver.com>
2025-10-31 12:30:18 +0900

    Title: 외판원 순회, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251031/외판원 순회/traveling-salesman-problem.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int INF = 987654321;
+
+int n;
+int A[10][10];
+int dp[14][(1 << 10)];
+
+int go(int idx,int nums){
+
+    if (nums == (1 << n) - 1) return A[idx][0];
+
+    int &ret = dp[idx][nums];
+    if (ret != -1) return ret;
+
+    ret = INF;    
+    for(int i=0;i<n;i++){
+        if (nums & (1 << i)) continue;
+        int cost = A[idx][i];
+        ret = min(ret, go(i,nums | (1 << i)) + cost);
+    }
+    return ret;
+}
+
+int main() {
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> A[i][j];
+        }
+    }
+    memset(dp,-1,sizeof(dp));
+    cout << go(0,0);
+
+    return 0;
+}
