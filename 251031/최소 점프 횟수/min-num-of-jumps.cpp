91090e7121624919711491a99f1507e3723174db
Chan <okc0202@naver.com>
2025-10-31 10:54:26 +0900

    Title: 최소 점프 횟수, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251031/최소 점프 횟수/min-num-of-jumps.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+const int INF = 987654321;
+
+int n;
+int num[10];
+int dp[14];
+
+int go(int idx){
+    
+    if (idx == n) return 0;
+
+    int &ret = dp[idx];
+    if (ret != -1) return ret;
+
+    int cur = num[idx];
+
+    ret = INF;
+    for(int i=1;i<=cur;i++){
+        ret = min(ret, go(idx+i) + 1);
+    }
+    return ret;
+}
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> num[i];
+    }
+    memset(dp,-1,sizeof(dp));
+    int ret = go(0);
+    if (ret == INF) cout << -1;
+    else cout << --ret;
+    return 0;
+}
