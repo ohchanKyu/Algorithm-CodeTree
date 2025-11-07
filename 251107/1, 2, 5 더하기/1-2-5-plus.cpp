628248892299a5608b733cffe54b6f7e2b1ba2a1
Chan <okc0202@naver.com>
2025-11-07 10:51:20 +0900

    Title: 1, 2, 5 더하기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251107/1, 2, 5 더하기/1-2-5-plus.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+const int mod = 10007;
+
+int N;
+int A[3] = {1,2,5};
+int dp[1004];
+
+int main() {
+    
+    cin >> N;
+    dp[0] = 1;
+    dp[1] = 1;
+
+    for(int i=2;i<=N;i++){
+        if (i < 5){
+            dp[i] = dp[i-1] + dp[i-2];
+        }else{
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-5];
+        }
+       
+    }
+    cout << dp[N];
+    return 0;
+}
