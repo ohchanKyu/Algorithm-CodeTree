338c9faaed4aabf19fae0974afaf11e0dcc7374c
Chan <okc0202@naver.com>
2025-11-07 10:16:04 +0900

    Title: 동전 거슬러주기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251107/동전 거슬러주기/coin-change.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+const int INF = 987654321;
+
+int N, M;
+int coin[100];
+int dp[10004];
+
+int main() {
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++) {
+        cin >> coin[i];
+    }
+    fill(dp, dp+10004, INF);
+    dp[0] = 0;
+    for(int i=0;i<N;i++){
+        int val = coin[i];
+        for(int j=val;j<=M;j++){
+            dp[j] = min(dp[j], dp[j - val] + 1);
+        }
+    }
+    if (dp[M] == INF) cout << -1;
+    else cout << dp[M];
+
+    return 0;
+}
